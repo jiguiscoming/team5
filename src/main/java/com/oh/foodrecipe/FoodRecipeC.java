1464acdba2a2a7521f37f3918b95887a44251698
seonyeop kim <kuissy09@gmali.com>
2022-06-13 12:11:41 +0900

    [add] 신규파일 추가

---
+++ src/main/java/com/oh/foodrecipe/FoodRecipeC.java
@@ -0,0 +1,26 @@
+package com.oh.foodrecipe;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FoodRecipeC")
+public class FoodRecipeC extends HttpServlet {
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RecipeDAO.getAllRecipe(request);
+	
+		request.setAttribute("contentPage", "foodRecipe/food_recipe.jsp");
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	
+	}
+
+}
